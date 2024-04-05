0a41a807e1daa850a430142ab0f829848e172086
sunday amos daniel <139694702+amosdan10@users.noreply.github.com>
2024-04-05 05:28:57 +0100

    Add files via upload

---
+++ data_types.dart
@@ -0,0 +1,83 @@
+//function that add two nmber
+import 'dart:io';
+
+void main() {
+  int addtwo() {
+    print("Enter first numer:");
+    //read the text from the standard input stream and store it two
+    int num1 = int.parse(stdin.readLineSync()!);
+    print("Enter second number:");
+    int num2 = int.parse(stdin.readLineSync()!);
+    int add = num1 + num2;
+    return add;
+  }
+
+  print(addtwo());
+}
+
+//function that sutract two nmber
+import 'dart:io';
+
+void main() {
+  int subtracttwo() {
+    print("Enter first numer:");
+    //read the text from the standard input stream and store it two
+    int num1 = int.parse(stdin.readLineSync()!);
+    print("Enter second number:");
+    int num2 = int.parse(stdin.readLineSync()!);
+    int subtract = num1 - num2;
+    return subtract;
+  }
+
+  print(subtracttwo());
+}
+//function that multiply two nmber
+import 'dart:io';
+
+void main() {
+  int multiplytwo() {
+    print("Enter first numer:");
+    //read the text from the standard input stream and store it two
+    int num1 = int.parse(stdin.readLineSync()!);
+    print("Enter second number:");
+    int num2 = int.parse(stdin.readLineSync()!);
+    int multiply = num1 * num2;
+    return multiply;
+  }
+
+  print(multiplytwo());
+}
+
+//function that divide two nmber
+import 'dart:io';
+
+void main() {
+  int dividetwo() {
+    print("Enter first numer:");
+    //read the text from the standard input stream and store it two
+    int num1 = int.parse(stdin.readLineSync()!);
+    print("Enter second number:");
+    int num2 = int.parse(stdin.readLineSync()!);
+    int divide = num1 + num2;
+    return divide;
+  }
+
+  print(dividetwo());
+}
+
+//function return stringlenth let
+import 'dart:io';
+
+void main() {
+  int stringlength() {
+    print("Enter first numer:");
+    //read the text from the standard input stream and store it two
+    int num1 = int.parse(stdin.readLineSync()!);
+    print("Enter second number:");
+    int num2 = int.parse(stdin.readLineSync()!);
+    int stringlength = num1 & num2;
+    return strringlenght;
+  }
+
+  print(stringlength());
+}
